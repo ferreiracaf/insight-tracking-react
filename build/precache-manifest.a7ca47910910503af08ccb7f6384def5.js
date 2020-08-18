00592dce6a731c7ee639beebf4b27db80748c339
ferreiracaf <carloscjr98@gmail.com>
2020-08-17 21:52:21 -0300

    testing deploy

---
+++ build/precache-manifest.a7ca47910910503af08ccb7f6384def5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9c419210db55ea3f39fc03fa782c64b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5aa5098cab3aa1f96417",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "f60033e3dbc7a35f7b0e",
+    "url": "/static/css/main.04d7df79.chunk.css"
+  },
+  {
+    "revision": "5aa5098cab3aa1f96417",
+    "url": "/static/js/2.931fc761.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.931fc761.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f60033e3dbc7a35f7b0e",
+    "url": "/static/js/main.375bb4fc.chunk.js"
+  },
+  {
+    "revision": "08c433fcb5127fbea784",
+    "url": "/static/js/runtime-main.1e090f78.js"
+  },
+  {
+    "revision": "f71d8f2b62854ee2169a9e11d2bc93f2",
+    "url": "/static/media/Insight_Avatar.f71d8f2b.png"
+  }
+]);
