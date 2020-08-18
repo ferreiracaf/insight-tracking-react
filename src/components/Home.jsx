9ea00df3c96e08c015b25c2513977a681f300317
ferreiracaf <carloscjr98@gmail.com>
2020-08-17 22:43:04 -0300

    deleting home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './img/Insight_Avatar.png'
+// import logo from './img/Insight_Avatar.png'
 
 export default class Home extends Component{
     render(){
